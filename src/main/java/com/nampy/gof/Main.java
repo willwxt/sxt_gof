601124ea760a29dc218cdb742b5db5b7192c9791
nampy <nampy001@163.com>
2020-01-05 13:14:35 +0800

    init sxt gof

---
+++ src/main/java/com/nampy/gof/Main.java
@@ -0,0 +1,12 @@
+package com.nampy.gof;
+
+/**
+ * @author nampy at 2020/1/5
+ * @Description:
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("sxtgof");
+    }
+}
