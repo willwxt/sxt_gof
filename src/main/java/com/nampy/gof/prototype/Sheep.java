1f9df9fde862e1cfda9a93c0d4dec471cae370c3
nampy <nampy001@163.com>
2020-01-13 17:52:15 +0800

    增加原型模式

---
+++ src/main/java/com/nampy/gof/prototype/Sheep.java
@@ -0,0 +1,29 @@
+package com.nampy.gof.prototype;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author nampy at 2020/1/13
+ * @description 1997年克隆羊多利
+ */
+@AllArgsConstructor
+@Data
+public class Sheep implements Cloneable {
+    private String sname;
+    private Date birthday;
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        //直接调用Object的clone方法!
+        Object obj = super.clone();
+
+        //添加如下代码实现深克隆deep clone, 如果没有就是浅克隆
+        Sheep s = (Sheep) obj;
+        s.birthday = (Date) this.birthday.clone(); //把属性也进行克隆
+
+        return obj;
+    }
+}
